72bcacfd6c46b1ef9a44724f506a7a46a046b8a3
ciscoeng98 <108091008+ciscoeng98@users.noreply.github.com>
2022-06-23 20:22:22 +0200

    Update settings.py

--- settings.py
+++ settings.py
@@ -8,10 +8,10 @@ DELAY=300 #time between each check in milisecond
 #####################
 ##     twilio     ###
 #####################
-TWILIO_PHONE_NUMBER='your twilio number here' #example: "+123456789"
-TARGET_PHONE_NUMBER='your own number here'      #example: "+123456789"
-ACCOUNT_SID = "your twilio SID here"    # Sample:  "AC3623abc255972fada59de3b3fjbed914"
-AUTH_TOKEN = "your twilio TOKEN here"   #Sample:  "8aac23abc255972fada59de3b3fjbed9" 
+TWILIO_PHONE_NUMBER='your twilio number here' #example: "+18573745946"
+TARGET_PHONE_NUMBER='your own number here'      #example: "+491794295910"
+ACCOUNT_SID = "your twilio SID here"    # Sample:  "ACdacbddf449ed7837c9cca7263f1ef231"
+AUTH_TOKEN = "your twilio TOKEN here"   #Sample:  "7785ccf816d639181e47a2b05de030d9" 
 
 
 
